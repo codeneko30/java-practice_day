06cecb35e366c786510fedb7471759b235830b93
code neko <codeneko30@gmail.com>
2026-09-03 23:33:45 +0600

    Initial Java setup

---
+++ day-1/hello-world.java
@@ -0,0 +1,85 @@
+ // print data
+ 
+ 
+ class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+ class helloSpring{
+    public static void main(String[] args) {
+        System.out.println("i am a new learner");
+    }
+}
+
+class turkey{
+    public static void main(String[] args){
+        System.out.println("i love turkey ");
+    }
+}
+
+class student{
+    public static void main(String[] args) {
+        System.out.println("robi is a talented student");
+    }
+}
+
+
+
+// variable
+
+class variableExample {
+    public static void main(String[] args) {
+
+        int age = 25;
+    String name = "kobir";
+
+    System.out.println(name);
+    System.out.println(age);
+
+    }
+    
+}
+
+
+
+class student_bio {
+    public static void main(String[] args) {
+        String name = "arif";
+        int class_name = 8;
+        int role = 1;
+        int age = 12;
+       String father_name = "abdullah";
+
+       System.out.println("Name:" + name);
+       System.out.println("Class:" + class_name);
+       System.out.println("Role:" + role);
+       System.out.println("Age:" + age);
+       System.out.println("Father name:" + father_name);
+       
+    }
+}
+
+
+
+
+//  Data Types
+
+class data_types{
+    public static void main(String[] args) {
+
+int number = 100;
+int negative = -50;
+System.out.println(number);      
+System.out.println(negative);
+
+    }
+}
+
+
+
+
+
+
+  
