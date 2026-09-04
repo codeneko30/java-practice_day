716e1496219fbab3d7400c33308683a570ba1f71
code neko <codeneko30@gmail.com>
2026-09-04 15:35:23 +0600

    Add arithmetic.java and day-1 practice code

---
+++ java/test.java
@@ -0,0 +1,19 @@
+ //  Data Types
+
+class data_types{
+    public static void main(String[] args) {
+
+int number = 100;
+int negative = -50;
+System.out.println(number);      
+System.out.println(negative);
+
+    }
+}
+
+
+
+
+
+
+  
