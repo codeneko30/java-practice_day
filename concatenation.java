b86927fd393867dbeb1d7f68e407660724e23979
code neko <codeneko30@gmail.com>
2026-09-05 13:01:00 +0600

    Organize Java practice files

--- concatenation.java
+++
@@ -1,8 +0,0 @@
-class concatenation{
-    public static void main(String args[]){
-        String firstName = "John";
-        String lastName = "Doe";
-        String fullName = firstName + " " + lastName;
-        System.out.println("Full Name: " + fullName);
-    }
-}
--- ternary.java
+++
@@ -1,7 +0,0 @@
-class ternary{
-    public static void main(String[] args){
-        int age = 20;
-        String eligibility = (age >= 18) ? " Adult" : " Minor";
-        System.out.println(eligibility);
-    }
-}
