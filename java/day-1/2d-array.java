c21b6528dc4ecc4bc88dbc113597d50274e40b97
code neko <codeneko30@gmail.com>
2026-09-07 21:27:23 +0600

    Organize Java day 1 and day 2 practice

---
+++ java/day-1/2d-array.java
@@ -0,0 +1,39 @@
+
+// example -1
+
+class simple_2d{
+    public static void main(String[] args){
+
+        int[][] numbers ={
+        {5, 10, 15},
+        {100, 200, 300},
+        {3, 9, 6}
+    };
+
+System.out.println(numbers[0][1]);
+System.out.println(numbers[2][2]);
+
+    }
+    
+
+    
+}
+
+
+// example-2 
+class seatingChart{
+    public static void main(String[] args) {
+        String[][] names = {
+            {"ali", "molla", "cina"},
+            {"moon", "mawhiba", "momin ali"},
+            {"yamal", "cubarsi"}
+        };
+
+
+        System.out.println(names[2][1]);
+    }
+
+
+
+
+}
